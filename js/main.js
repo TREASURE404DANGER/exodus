dc533c7611302c9a297281de737e21858731a499
Conphide <conphide.tech@gmail.com>
2025-11-18 14:40:27 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,3 +14,57 @@ mobileLinks.forEach((link) => {
     sideButton.classList.remove("active");
   });
 });
+
+const headerWords = [
+  "WhatsApp Bot",
+  "Telegram Bot",
+  "Discord Bot",
+  "Game Server",
+  "AI Agent",
+  "Cron Job",
+  "Database",
+  "Web App/API",
+  "Webhook Receiver",
+  "Automation Script",
+];
+
+const rotatingWordElement = document.getElementById("rotatingWord");
+
+function rotateWords() {
+  let currentIndex = 0;
+
+  currentIndex = Math.floor(Math.random() * headerWords.length);
+  rotatingWordElement.textContent = headerWords[currentIndex];
+
+  setInterval(() => {
+    currentIndex = (currentIndex + 1) % headerWords.length;
+
+    // Effect
+    rotatingWordElement.style.opacity = "0";
+
+    setTimeout(() => {
+      rotatingWordElement.textContent = headerWords[currentIndex];
+      rotatingWordElement.style.opacity = "1";
+    }, 300);
+  }, 3000);
+}
+
+//Effect
+rotatingWordElement.style.transition = "opacity 0.3s ease";
+
+rotateWords();
+
+const heroImages = [
+  "images/hero-1.png",
+  "images/hero-2.png",
+  "images/hero-3.png",
+  "images/hero-4.png",
+];
+
+function setRandomHeroImage() {
+  const heroImgElement = document.getElementById("heroImg");
+  const randomIndex = Math.floor(Math.random() * heroImages.length);
+  heroImgElement.src = heroImages[randomIndex];
+}
+
+setRandomHeroImage();
